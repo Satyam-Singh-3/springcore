b644caecc15b5f09292b3be360f33b2501e5842e
tectoro <satyam2707singh@gmail.com>
2022-06-03 11:26:47 +0530

    spring security

---
+++ Spring-security-ldap/src/test/java/com/spring/securty/ldap/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.securty.ldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
